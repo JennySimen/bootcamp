40490cff265fef69a102fd1449ad630f06a2e0f5
JennySimen <yakamjennifer@gmail.com>
2021-02-04 19:17:49 +0100

    fixed automatic reply issue

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const request = require('request')
 const bodyParser = require('body-parser')
+const port = process.env.PORT || 3000;
 
 const app = express()
 app.use(bodyParser.urlencoded( { extended: false}))
