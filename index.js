9fa78351f9e2b1a3ffd0c209554dd4c1b6417ce0
JennySimen <yakamjennifer@gmail.com>
2021-02-05 11:13:07 +0100

    searching for the error

--- index.js
+++ index.js
@@ -65,190 +65,187 @@ app.post('/webhook', function(req, res){
     res.sendStatus(200);
 });
 
-// function buttonMenu(sender) {
-//     const resonseData = `{
-//           "attachment":{
-//             "type":"template",
-//             "payload":{
-//               "template_type":"button",
-//               "text":"Select a Category",
-//               "buttons":[
-//                 {
-//                     "type":"postback",
-//                     "title":"Food",
-//                     "payload":"Food"
-//                 },
-//                 {
-//                     "type":"postback",
-//                     "title":"Fast food",
-//                     "payload":"Fast food"
-//                 },
-//                 {
-//                     "type":"postback",
-//                     "title":"Juice",
-//                     "payload":"Juice"
-//                 }
-//               ]
-//             }
-//           }
-//       }`;
+function buttonMenu(sender) {
+    const resonseData = `{
+          "attachment":{
+            "type":"template",
+            "payload":{
+              "template_type":"button",
+              "text":"Select a Category",
+              "buttons":[
+                {
+                    "type":"postback",
+                    "title":"Food",
+                    "payload":"Food"
+                },
+                {
+                    "type":"postback",
+                    "title":"Fast food",
+                    "payload":"Fast food"
+                },
+                {
+                    "type":"postback",
+                    "title":"Juice",
+                    "payload":"Juice"
+                }
+              ]
+            }
+          }
+      }`;
 
-//       request({
-//         url: "https://graph.facebook.com/v2.6/me/messages",
-//         qs: { access_token: ACCESS_TOKEN },
-//         method: "POST",
-//         json: {
-//             recipient: { id: sender },
-//             message: resonseData
-//         }
-//     },function (error, response, body) {
-//         if (error) {
-//             console.log("Sending error", error);
-//         } else if (response.body.error) {
-//             console.log("response body error", response.body.error);
-//         }
-//     });
-// } 
-// function juiceMenu(sender) {
-//     const firstData = `{
-//         "attachment": {
-//             "type": "template",
-//             "payload": {
-//                "template_type": "generic",
-//                "elements": [
-//                 {"foodName":"Chicken and Fries", "foodType":"fast food", "protein":"chicken"},
-//                ]
-//             }
-//           }
-//       }`;
-
-//     const secondData = `{
-//         "attachment": {
-//             "type": "template",
-//             "payload": {
-//                "template_type": "generic",
-//                "elements": [
-//                 {"foodName":"Fried rice", "foodType":"Food", "protein":"chicken"},
-//                ]
-//             }
-//           }
-//       }`;
-
-//     const thirdData = `{
-//         "attachment": {
-//             "type": "template",
-//             "payload": {
-//                "template_type": "generic",
-//                "elements": [
-//                 {"foodName":"Milkshake", "foodType":"Juice", "protein":"milk"}
-//                ]
-//             }
-//           }
-//       }`;
-//       const resonseData = `{
-//         "attachment": {
-//             "type": "template",
-//             "payload": {
-//                "template_type": "media",
-//                "elements": [
-//                 {
-//                     {"foodName":"Ndole and plantain", "foodType":"food", "protein":"milk"}
-//                  }
-//                      ]
-//                   }
-//                ]
-//             }
-//           }
-//       }`;
-//       request({
-//         url: "https://graph.facebook.com/v2.6/me/messages",
-//         qs: { access_token: ACCESS_TOKEN },
-//         method: "POST",
-//         json: {
-//             recipient: { id: sender },
-//             message: resonseData
-//         }
-//     }, function (error, response, body) {
-//         if (error) {
-//             console.log("Sending error", error);
-//         } else if (response.body.error) {
-//             console.log("response body error", response.body.error);
-//         }
-//     });
-// }
-// function sendText(sender, text) {
-//     let message_data = { text: text };
-//     request({
-//         url: "https://graph.facebook.com/v2.6/me/messages",
-//         qs: { access_token: ACCESS_TOKEN },
-//         method: "POST",
-//         json: {
-//             recipient: { id: sender },
-//             message: message_data
-//         }
-//     }, function (error, response, body) {
-//         if (error) {
-//             console.log("Sending error");
-//         } else if (response.body.error) {
-//             console.log("response body error");
-//         }
-//     });
-// }
-// function IsJsonString(str) {
-//     try {
-//         JSON.parse(str);
-//     } catch (e) {
-//         return false;
-//     }
-//     return true;
-// }
-
-// // app.listen(port, () => console.log('The app is running on port ' + port));
-        
-        
-function processPostback(event) {
-    let senderId = event.sender.id;
-    let payload = event.postback.payload;
-
-    if (payload === "Greeting") {
-      // Get user's first name from the User Profile API
-      // and include it in the greeting
       request({
-        url: "https://graph.facebook.com/v2.6/" + senderId,
-        qs: {
-          access_token: process.env.PAGE_ACCESS_TOKEN,
-          fields: "first_name"
-        },
-        method: "GET"
-      }, function(error, response, body) {
-        let greeting = "";
-        if (error) {
-          console.log("Error getting user's name: " +  error);
-        } else {
-          let bodyObj = JSON.parse(body);
-          name = bodyObj.first_name;
-          greeting = "Hi " + name + ". ";
+        url: "https://graph.facebook.com/v2.6/me/messages",
+        qs: { access_token: ACCESS_TOKEN },
+        method: "POST",
+        json: {
+            recipient: { id: sender },
+            message: resonseData
         }
-        let message = greeting + "This is a test bot i am building for my bot development practice";
-        sendMessage(senderId, {text: message});
-      });
-    }
-  }
-
-  // sends message to user
-function sendMessage(recipientId, message) {
-    request({
-      url: "https://graph.facebook.com/v2.6/me/messages",
-      qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
-      method: "POST",
-      json: {
-        recipient: {id: recipientId},
-        message: message,
-      }
-    }, function(error, response, body) {
-      if (error) {
-        console.log("Error sending message: " + response.error);
-      }
+    },function (error, response, body) {
+        if (error) {
+            console.log("Sending error", error);
+        } else if (response.body.error) {
+            console.log("response body error", response.body.error);
+        }
     });
-  }
+} 
+function juiceMenu(sender) {
+    const firstData = `{
+        "attachment": {
+            "type": "template",
+            "payload": {
+               "template_type": "generic",
+               "elements": [
+                {"foodName":"Chicken and Fries", "foodType":"fast food", "protein":"chicken"},
+               ]
+            }
+          }
+      }`;
+
+    const secondData = `{
+        "attachment": {
+            "type": "template",
+            "payload": {
+               "template_type": "generic",
+               "elements": [
+                {"foodName":"Fried rice", "foodType":"Food", "protein":"chicken"},
+               ]
+            }
+          }
+      }`;
+
+    const thirdData = `{
+        "attachment": {
+            "type": "template",
+            "payload": {
+               "template_type": "generic",
+               "elements": [
+                {"foodName":"Milkshake", "foodType":"Juice", "protein":"milk"}
+               ]
+            }
+          }
+      }`;
+      const resonseData = `{
+        "attachment": {
+            "type": "template",
+            "payload": {
+               "template_type": "media",
+               "elements": [
+                    {"foodName":"Ndole and plantain", "foodType":"food", "protein":"milk"}
+                     ]
+                  }
+            }
+          }
+      }`;
+      request({
+        url: "https://graph.facebook.com/v2.6/me/messages",
+        qs: { access_token: ACCESS_TOKEN },
+        method: "POST",
+        json: {
+            recipient: { id: sender },
+            message: resonseData
+        }
+    }, function (error, response, body) {
+        if (error) {
+            console.log("Sending error", error);
+        } else if (response.body.error) {
+            console.log("response body error", response.body.error);
+        }
+    });
+}
+function sendText(sender, text) {
+    let message_data = { text: text };
+    request({
+        url: "https://graph.facebook.com/v2.6/me/messages",
+        qs: { access_token: ACCESS_TOKEN },
+        method: "POST",
+        json: {
+            recipient: { id: sender },
+            message: message_data
+        }
+    }, function (error, response, body) {
+        if (error) {
+            console.log("Sending error");
+        } else if (response.body.error) {
+            console.log("response body error");
+        }
+    });
+}
+function IsJsonString(str) {
+    try {
+        JSON.parse(str);
+    } catch (e) {
+        return false;
+    }
+    return true;
+}
+
+// app.listen(port, () => console.log('The app is running on port ' + port));
+        
+        
+// function processPostback(event) {
+//     let senderId = event.sender.id;
+//     let payload = event.postback.payload;
+
+//     if (payload === "Greeting") {
+//       // Get user's first name from the User Profile API
+//       // and include it in the greeting
+//       request({
+//         url: "https://graph.facebook.com/v2.6/" + senderId,
+//         qs: {
+//           access_token: process.env.PAGE_ACCESS_TOKEN,
+//           fields: "first_name"
+//         },
+//         method: "GET"
+//       }, function(error, response, body) {
+//         let greeting = "";
+//         if (error) {
+//           console.log("Error getting user's name: " +  error);
+//         } else {
+//           let bodyObj = JSON.parse(body);
+//           name = bodyObj.first_name;
+//           greeting = "Hi " + name + ". ";
+//         }
+//         let message = greeting + "This is a test bot i am building for my bot development practice";
+//         sendMessage(senderId, {text: message});
+//       });
+//     }
+//   }
+
+//   // sends message to user
+// function sendMessage(recipientId, message) {
+//     request({
+//       url: "https://graph.facebook.com/v2.6/me/messages",
+//       qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+//       method: "POST",
+//       json: {
+//         recipient: {id: recipientId},
+//         message: message,
+//       }
+//     }, function(error, response, body) {
+//       if (error) {
+//         console.log("Error sending message: " + response.error);
+//       }
+//     });
+//   }
   
